532f2b4a76e3b7543ac547540550412e2377675b
Kristian Løvstrøm <kristian@loevstroem.dk>
2022-04-14 16:19:54 +0200

    Initial commit

---
+++ header.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<?php wp_head(); ?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap" rel="stylesheet">
+</head>
+
+<?php
+	$navbar_scheme   = get_theme_mod( 'navbar_scheme', 'navbar-light bg-light' ); // Get custom meta-value.
+	$navbar_position = get_theme_mod( 'navbar_position', 'static' ); // Get custom meta-value.
+
+	$search_enabled  = get_theme_mod( 'search_enabled', '1' ); // Get custom meta-value.
+?>
+
+<body <?php body_class(); ?>>
+
+<?php wp_body_open(); ?>
+
+<a href="#main" class="visually-hidden-focusable"><?php esc_html_e( 'Skip to main content', 'greeting2' ); ?></a>
+
+<div id="wrapper">
+	<header>
+        <div class="bg-pink small text-uppercase">
+            <div class="container d-flex justify-content-between">
+                <ul class="nav d-inline-flex m-0">
+                    <li class="nav-item">
+                        <a class="nav-link text-dark ps-0" href="#">Bliv butikspartner</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Cases</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Om Greeting</a>
+                    </li>
+                </ul>
+                <ul class="nav d-inline-flex m-0">
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="mailto:kontakt@greeting.dk">kontakt@greeting.dk</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="tel:61462512">61462512</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!--
+		<nav id="header" class="navbar navbar-expand-md <?php echo esc_attr( $navbar_scheme ); if ( isset( $navbar_position ) && 'fixed_top' === $navbar_position ) : echo ' fixed-top'; elseif ( isset( $navbar_position ) && 'fixed_bottom' === $navbar_position ) : echo ' fixed-bottom'; endif; if ( is_home() || is_front_page() ) : echo ' home'; endif; ?>">
+			<div class="container">
+				<a class="navbar-brand" href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<?php
+						$header_logo = get_theme_mod( 'header_logo' ); // Get custom meta-value.
+
+						if ( ! empty( $header_logo ) ) :
+					?>
+						<img src="<?php echo esc_url( $header_logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
+					<?php
+						else :
+							echo esc_attr( get_bloginfo( 'name', 'display' ) );
+						endif;
+					?>
+				</a>
+
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'greeting2' ); ?>">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div id="navbar" class="collapse navbar-collapse">
+					<?php
+						// Loading WordPress Custom Menu (theme_location).
+						wp_nav_menu(
+							array(
+								'theme_location' => 'main-menu',
+								'container'      => '',
+								'menu_class'     => 'navbar-nav me-auto',
+								'fallback_cb'    => 'WP_Bootstrap_Navwalker::fallback',
+								'walker'         => new WP_Bootstrap_Navwalker(),
+							)
+						);
+
+						if ( '1' === $search_enabled ) :
+					?>
+							<form class="search-form my-2 my-lg-0" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+								<div class="input-group">
+									<input type="text" name="s" class="form-control" placeholder="<?php esc_attr_e( 'Search', 'greeting2' ); ?>" title="<?php esc_attr_e( 'Search', 'greeting2' ); ?>" />
+									<button type="submit" name="submit" class="btn btn-outline-secondary"><?php esc_html_e( 'Search', 'greeting2' ); ?></button>
+								</div>
+							</form>
+					<?php
+						endif;
+					?>
+				</div>
+			</div>
+		</nav>
+	    -->
+</header>
+
+	<main id="main" class="container"<?php if ( isset( $navbar_position ) && 'fixed_top' === $navbar_position ) : echo ' style="padding-top: 100px;"'; elseif ( isset( $navbar_position ) && 'fixed_bottom' === $navbar_position ) : echo ' style="padding-bottom: 100px;"'; endif; ?>>
+		<?php
+			// If Single or Archive (Category, Tag, Author or a Date based page).
+			if ( is_single() || is_archive() ) :
+		?>
+			<div class="row">
+				<div class="col-12">
+		<?php
+			endif;
+		?>
---
+++ page.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Page (Default)
+ * Description: Page template.
+ *
+ */
+
+get_header();
+
+the_post();
+?>
+<div class="row">
+	<div class="col-12">
+		<div id="post-<?php the_ID(); ?>" <?php post_class( 'content' ); ?>>
+            <?php if(!is_front_page()): ?>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+            <?php endif; ?>
+			<?php
+				the_content();
+
+				wp_link_pages(
+					array(
+						'before' => '<div class="page-links">' . __( 'Pages:', 'greeting2' ),
+						'after'  => '</div>',
+					)
+				);
+				edit_post_link( esc_html__( 'Edit', 'greeting2' ), '<span class="edit-link">', '</span>' );
+			?>
+		</div><!-- /#post-<?php the_ID(); ?> -->
+		<?php
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+		?>
+	</div><!-- /.col -->
+</div><!-- /.row -->
+<?php
+get_footer();
