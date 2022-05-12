a8dec05762450d4d4dac03da5e75a322efb5fcb5
Dennis Lauritzen <dennis@idemedia.dk>
2022-05-12 22:06:31 +0200

    Frontpage updates

--- front-page.php
+++ front-page.php
@@ -622,16 +622,8 @@ if(!empty($results)){
 
 
 <?php
-var_dump($this_post_id);
-var_dump(get_field('frontpage_full_width_boxes', $this_post_id));
 if (have_rows('frontpage_full_width_boxes', $this_post_id) ){
-  while (have_posts()) : the_post();
-
-
-  endwhile;
-
-
-  while( have_rows('frontpage_full_width_boxes', $post_id)) : the_row();
+  while( have_rows('frontpage_full_width_boxes', $this_post_id)) : the_row();
     $image = get_sub_field('image');
     $heading = get_sub_field('heading');
     $text = get_sub_field('text');
@@ -716,66 +708,6 @@ if (have_rows('frontpage_full_width_boxes', $this_post_id) ){
   endwhile;
 }
 ?>
-<section id="about">
-  <div class="container-fluid">
-    <div class="row">
-        <div class="col-12 col-md-6 text-center d-flex align-items-center bg-teal text-white" style="height: 35vw; min-height: 500px;">
-            <div class="p-2 p-lg-5 p-xl-5">
-              <div class="media-text__inner text-right px-lg-4 px-xl-4">
-                <svg xmlns="http://www.w3.org/2000/svg" class="pb-3" width="183" height="72" viewBox="0 0 183 72">
-                  <g stroke="#F7F4F4" stroke-width="2" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M35.696 51.159H2.652v18.174c0 .912.74 1.652 1.652 1.652h29.74c.911 0 1.652-.74 1.652-1.652V51.159zm1.652 0H1V44.55c0-.912.74-1.652 1.652-1.652h33.044c.912 0 1.652.74 1.652 1.652v6.609zm-18.174-8.261v28.087"></path><path d="M19.138 42.898c.757-4.274 4.288-9.115 8.643-9.857 1.867-.319 2.959 1.279 2.959 2.926 0 .717-.212 1.375-.562 1.888v-.006c-2.265 3.6-11.04 5.049-11.04 5.049"></path><path d="M19.21 42.898c-.757-4.274-4.288-9.115-8.643-9.857-1.867-.319-2.959 1.279-2.959 2.926 0 .717.212 1.375.562 1.888v-.006c2.265 3.6 11.04 5.049 11.04 5.049m158.877 27.45h-37.174A3.912 3.912 0 01137 66.435V44.913A3.912 3.912 0 01140.913 41h37.174A3.912 3.912 0 01182 44.913v21.522a3.912 3.912 0 01-3.913 3.913h0zm-35.217-9.783h7.826m-7.826 3.913h13.695"></path><path d="M155.013 47.707a9.785 9.785 0 009.378 6.988 9.785 9.785 0 00-6.989-9.377c-1.426-.425-2.813.962-2.388 2.389h0zm18.757 0a9.785 9.785 0 01-9.379 6.988 9.785 9.785 0 016.99-9.377c1.425-.425 2.813.962 2.388 2.389h0zm-9.379 22.641V41m0 13.696l-5.87 5.87m5.87-5.87l5.87 5.87M137 54.696h45m-61.899-21.034H53.848a3.314 3.314 0 01-3.313-3.313v-9.938a3.314 3.314 0 013.313-3.313H120.1a3.314 3.314 0 013.313 3.313v9.938a3.314 3.314 0 01-3.313 3.313h0z"></path><path d="M113.476 70.101H60.473a6.624 6.624 0 01-6.625-6.625V33.662H120.1v29.814a6.624 6.624 0 01-6.625 6.625h0zM71.097 5.265c2.034 6.844 8.374 11.833 15.877 11.833 0-7.503-4.989-13.844-11.833-15.877-2.415-.72-4.763 1.63-4.044 4.044h0zm31.755 0c-2.033 6.844-8.374 11.833-15.877 11.833 0-7.503 4.989-13.844 11.833-15.877 2.415-.72 4.763 1.63 4.044 4.044h0z"></path>
-                    <path d="M86.975 70.101V17.098l-9.938 9.939m9.938-9.939l9.938 9.939"></path>
-                  </g>
-                </svg>
-                <h3 class="py-2">Om Greeting.dk</h3>
-                <p class="px-lg-5 px-xl-5">👋🏼👋🏼 Vi er Lisette og Dennis, og vi står bag Greeting.dk! </p>
-                <p class="px-lg-5 px-xl-5">Vi vil skabe de allerbedste muligheder for at sende fine og personlige gaver fra landets fysiske butikker til modtagere i hele Danmark.</p>
-                <a class="btn bg-white text-teal rounded-pill mt-2 py-3 px-4" href="<?php echo site_url(); ?>/om-os/" title="Om Greeting.dk">Om Greeting.dk ></a>
-              </div>
-            </div>
-          </div>
-        <div class="col-12 col-md-6" style="background-image:url('https://greeting.dk/wp-content/uploads/2021/11/265A7587-aspect-ratio-800-700.jpg');background-size:cover;"></div>
-    </div>
-  </div>
-</section>
-
-<section class="businessgifts">
-  <div class="container-fluid">
-    <div class="row">
-        <div class="col-12 col-md-6" style="background-image:url('https://greeting.dk/wp-content/uploads/2021/11/DSC_0628-aspect-ratio-800-700.jpg');background-size:cover;"></div>
-        <div class="col-12 col-md-6 text-center d-flex align-items-center bg-pink text-teal" style="height: 35vw; min-height: 500px;">
-          <div class="p-2 p-lg-5 p-xl-5">
-            <div class="media-text__inner text-right px-lg-4 px-xl-4">
-              <span class="small">Medarbejder- eller kundegaver?</span>
-              <h2 class="py-2">Send firmagaver fra lokale butikker</h2>
-              <p class="px-lg-5 px-xl-5">Lad os sørge for, at dine medarbejdere, kunder eller forretningsforbindelser modtager den perfekte gavehilsen fra dig!</p>
-              <p class="px-lg-5 px-xl-5">Når du køber gaver via Greeting.dk, så støtter du og din virksomhed samtidigt de lokale specialbutikker. </p>
-              <a class="btn bg-teal text-white rounded-pill mt-2 py-3 px-4" href="<?php echo site_url(); ?>/firmagaver/" title="Køb firma- &amp; medarbejderagaver" target="">Læs om firmagaver ></a>
-            </div>
-          </div>
-        </div>
-    </div>
-  </div>
-</section>
-
-<section class="partner">
-  <div class="container-fluid">
-    <div class="row">
-        <div class="col-12 col-md-6 text-center d-flex align-items-center bg-yellow text-teal" style="height: 35vw; min-height: 500px;">
-            <div class="p-2 p-lg-5 p-xl-5">
-                <div class="media-text__inner text-left px-lg-4 px-xl-4">
-                    <span class="small">Er du butik?</span>
-                    <h2 class="py-2">Der er mange fordele for dig som vores samarbejdspartner</h2>
-                    <p class="px-lg-5 px-xl-5">Greeting.dk samarbejder med mange forskellige fysiske specialbutikker i Danmark – og det eneste det kræver er, at du forhandler produkter med gave-potentiale.</p>
-                    <a class="btn bg-white rounded-pill mt-2 py-3 px-4" href="<?php echo site_url(); ?>/bliv-butikspartner" title="Bliv partner" target="">Bliv partner ></a>
-                </div>
-            </div>
-        </div>
-        <div class="col-12 col-md-6" style="background-image:url('https://greeting.dk/wp-content/uploads/2021/11/265A7587-aspect-ratio-800-700.jpg');background-size:cover;"></div>
-    </div>
-  </div>
-</section>
 
 <section id="didyouknow" class="bg-light-grey pb-4">
   <div class="container-fluid text-center mb-3 py-5">
