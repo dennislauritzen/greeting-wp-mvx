2494fbb71ba356eeb0db8175331347d17cd8962d
Dennis Lauritzen <dennis@idemedia.dk>
2022-12-13 11:24:27 +0100

    Changes the mail back to original language

--- dc-product-vendor/emails/vendor-new-order.php
+++ dc-product-vendor/emails/vendor-new-order.php
@@ -40,7 +40,7 @@ $qrcode = 'https://chart.googleapis.com/chart?chs=135x135&cht=qr&chl='.$codeCont
   $main_order_id = $main_order->ID;
 ?>
 <p>Ordrenr.: #<?php echo $main_order_id; ?> (Sub-ordre ID: #<?php echo $order->get_id(); ?>)</p>
-<p><?php printf(esc_html__('A new order was received from %s. Their order is as follows:', 'dc-woocommerce-multi-vendor'), $order->get_billing_first_name() . ' ' . $order->get_billing_last_name()); ?></p>
+<p><p><?php printf(esc_html__('A new order was received and marked as %s from %s. Their order is as follows:', 'dc-woocommerce-multi-vendor'), $order->get_status( 'edit' ), $order->get_billing_first_name() . ' ' . $order->get_billing_last_name()); ?></p></p>
 
 <?php do_action('woocommerce_email_before_order_table', $order, true, false, $email); ?>
 <table cellspacing="0" cellpadding="6" style="width: 100%; border: 1px solid #eee;" border="1" bordercolor="#eee">
