87155472e48c0d0d169c0732835e65df7cd917a9
Dennis Lauritzen <dennis@idemedia.dk>
2023-07-26 17:00:33 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -37,23 +37,23 @@
 		      <div class="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-2 col-xl-2">
 		        <h6 class="pb-3 text-uppercase">Udvalgte postnumre i Jylland</h6>
 		        <ul class="list-unstyled mb-0">
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/6000-kolding/" class="text-white">6000 Kolding</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/6100-haderslev/" class="text-white">6100 Haderslev</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/6700-esbjerg/" class="text-white">6700 Esbjerg</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/6710-esbjerg-v/" class="text-white">6710 Esbjerg V</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/7000-fredericia/" class="text-white">7000 Fredericia</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/7100-vejle/" class="text-white">7100 Vejle</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/7400-herning/" class="text-white">7400 Herning</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/7500-holstebro/" class="text-white">7500 Holstebro</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/8000-aarhus-c/" class="text-white">8000 Aarhus C</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/8200-aarhus-n/" class="text-white">8200 Aarhus N</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/8240-risskov/" class="text-white">8240 Risskov</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/8600-silkeborg/" class="text-white">8600 Silkeborg</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/8800-viborg/" class="text-white">8800 Viborg</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/8900-randers-c/" class="text-white">8900 Randers C</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/8920-randers-nv/" class="text-white">8920 Randers NV</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/9000-aalborg/" class="text-white">9000 Aalborg</a></li>
-                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/9800-hjoerring/" class="text-white">9800 Hjørring</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/6000-kolding/" aria-label="Gå til bysiden for 6000 Kolding" class="text-white">6000 Kolding</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/6100-haderslev/" aria-label="Gå til bysiden for 6100 Haderslev" class="text-white">6100 Haderslev</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/6700-esbjerg/" aria-label="Gå til bysiden for 6700 Esbjerg" class="text-white">6700 Esbjerg</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/6710-esbjerg-v/" aria-label="Gå til bysiden for 6710 Esbjerg V" class="text-white">6710 Esbjerg V</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/7000-fredericia/" aria-label="Gå til bysiden for 7000 Fredericia" class="text-white">7000 Fredericia</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/7100-vejle/" aria-label="Gå til bysiden for 7100 Vejle" class="text-white">7100 Vejle</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/7400-herning/" aria-label="Gå til bysiden for 7400 Herning" class="text-white">7400 Herning</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/7500-holstebro/" aria-label="Gå til bysiden for 7500 Holstebro" class="text-white">7500 Holstebro</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/8000-aarhus-c/" aria-label="Gå til bysiden for 8000 Aarhus C" class="text-white">8000 Aarhus C</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/8200-aarhus-n/" aria-label="Gå til bysiden for 8200 Aarhus N" class="text-white">8200 Aarhus N</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/8240-risskov/" aria-label="Gå til bysiden for 8240 Risskov" class="text-white">8240 Risskov</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/8600-silkeborg/" aria-label="Gå til bysiden for 8600 Silkeborg" class="text-white">8600 Silkeborg</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/8800-viborg/" aria-label="Gå til bysiden for 8800 Viborg" class="text-white">8800 Viborg</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/8900-randers-c/" aria-label="Gå til bysiden for 8900 Randers C" class="text-white">8900 Randers C</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/8920-randers-nv/" aria-label="Gå til bysiden for 8920 Randers NV" class="text-white">8920 Randers NV</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/9000-aalborg/" aria-label="Gå til bysiden for 9000 Aalborg" class="text-white">9000 Aalborg</a></li>
+                    <li class="pb-1"><a href="<?php echo site_url(); ?>/c/9800-hjoerring/" aria-label="Gå til bysiden for 9800 Hjørring" class="text-white">9800 Hjørring</a></li>
 		        </ul>
 		      </div>
 		      <div class="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-3 col-xl-3">
@@ -132,25 +132,25 @@
 		          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#1b4949" class="me-1 bi bi-envelope" viewBox="0 0 16 16">
 								<path fill-rule="evenodd" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l3.235 1.94a2.76 2.76 0 0 0-.233 1.027L1 5.384v5.721l3.453-2.124c.146.277.329.556.55.835l-3.97 2.443A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741l-3.968-2.442c.22-.28.403-.56.55-.836L15 11.105V5.383l-3.002 1.801a2.76 2.76 0 0 0-.233-1.026L15 4.217V4a1 1 0 0 0-1-1H2Zm6 2.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
 							</svg>
-		          <a href="mailto:<?php echo get_field('greeting_contact_mail_address', 'option'); ?>" class="text-white" style="font-size: 0.9em;"><?php echo get_field('greeting_contact_mail_address', 'option'); ?></a>
+		          <a href="mailto:<?php echo get_field('greeting_contact_mail_address', 'option'); ?>" aria-label="Skriv en mail til Greeting.dk" class="text-white" style="font-size: 0.9em;"><?php echo get_field('greeting_contact_mail_address', 'option'); ?></a>
 		        </p>
 		        <p>
 		          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#1b4949" class="me-1 bi bi-telephone" viewBox="0 0 16 16">
 		          <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
 		          </svg>
-		            <a href="tel:+45<?php echo get_field('greeting_contact_phone_number', 'option'); ?>" class="text-white" style="font-size: 0.9em;">(+45) <?php echo trim(strrev(chunk_split(strrev( get_field('greeting_contact_phone_number', 'option') ),2, ' '))); ?></a>
+		            <a href="tel:+45<?php echo get_field('greeting_contact_phone_number', 'option'); ?>" aria-label="Ring til Greeting.dk" class="text-white" style="font-size: 0.9em;">(+45) <?php echo trim(strrev(chunk_split(strrev( get_field('greeting_contact_phone_number', 'option') ),2, ' '))); ?></a>
 		        </p>
                 <div class="">
                     <ul class="social m-0 p-0 mt-2">
                         <li class="me-1 me-md-2">
-                            <a href="https://www.facebook.com/greeting.dk" class="text-white">
+                            <a href="https://www.facebook.com/greeting.dk" aria-label="Besøg Greeting.dks Facebook-profil" class="text-white">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="float-end bi bi-facebook" viewBox="0 0 18 18">
                                     <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
                                 </svg>
                             </a>
                         </li>
                         <li class="me-1 me-md-2">
-                            <a href="https://www.instagram.com/greeting.dk/">
+                            <a href="https://www.instagram.com/greeting.dk/" aria-label="Besøg Greeting.dks Instagram-profil">
                                 <figure class="rounded-circle text-center" style="background-color: #ffffff; width: 40px; height: 40px; padding: 7px;">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="#446a6b" class="bi bi-instagram" viewBox="0 0 16 16">
                                         <path color="#446a6b" d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
