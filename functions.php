065ca16d446eb244e926aeab8e38d294393b3df4
Dennis Lauritzen <dennis@idemedia.dk>
2023-12-19 14:20:50 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -5297,9 +5297,45 @@ function add_links_to_keywords($content, $taxonomies) {
         }
     }
 
-
     // Apply wpautop to preserve line breaks
     $content = wpautop($content);
 
     return $content;
 }
+
+/* Register vendor ID in REST API */
+// Make sure to use PHP >= 5.4
+add_action(
+    'rest_api_init',
+    function () {
+        // Field name to register.
+        $field = 'vendor_id';
+        register_rest_field(
+            'post',
+            $field,
+            array(
+                'get_callback'    => function ( $object ) use ( $field ) {
+                    // Get field as single value from post meta.
+                    return get_post_meta( $object['id'], $field, true );
+                },
+                'update_callback' => function ( $value, $object ) use ( $field ) {
+                    // Update the field/meta value.
+                    update_post_meta( $object->ID, $field, $value );
+                },
+                'schema'          => array(
+                    'type'        => 'string',
+                    'arg_options' => array(
+                        'sanitize_callback' => function ( $value ) {
+                            // Make the value safe for storage.
+                            return sanitize_text_field( $value );
+                        },
+                        'validate_callback' => function ( $value ) {
+                            // Valid if it contains exactly 10 English letters.
+                            return (bool) preg_match( '/\A[a-z]{10}\Z/', $value );
+                        },
+                    ),
+                ),
+            )
+        );
+    }
+);
