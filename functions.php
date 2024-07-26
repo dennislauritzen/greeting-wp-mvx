3b54a8a10fc9e75a1f877f7357e4b79e84e9206e
Dennis Lauritzen <dennis@idemedia.dk>
2024-07-26 12:41:54 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -667,7 +667,7 @@ function greeting3_scripts_loader() {
 
     wp_enqueue_style(
         'wpb-google-fonts',
-        'https://fonts.googleapis.com/css2?family=Dela+Gothic+One:wght@300;500&family=Inter:wght@300;500;700&family=Rubik:wght@300;500;700&display=swap',
+        'https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=Inter:wght@300;500;700&family=Rubik:wght@300;500;700&display=swap',
         array(),
         $theme_version,
         'all'
