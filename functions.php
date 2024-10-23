d79fdaaf3cc786ad84cd01209aebe26cc4f335e4
Dennis Lauritzen <dennis@idemedia.dk>
2024-10-24 00:15:52 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1002,7 +1002,7 @@ include('functions-parts/admin.general.functions.php');
 include('functions-parts/admin.order.functions.php');
 include('functions-parts/admin.product.functions.php');
 include('functions-parts/admin.vendor.functions.php');
-include('functions-parts/admin.vendor.invite.functions.php');
+#include('functions-parts/admin.vendor.invite.functions.php');
 include('functions-parts/admin.acf.blocks.functions.php');
 
 ## Include functions regarding checkout.
